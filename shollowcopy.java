0e3279b5ee4aeceeaaef49cfc297e39a14ae8f03
saifullah <saifullahahad0@gmail.com>
2026-08-23 20:59:02 +0530

    shollow copy code

---
+++ shollowcopy.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class shollowcopy{
+    public static void main(String[] args){
+        
+            int[] arr =  {1,22, 3,4,6,7};
+            int[] y = arr;
+            y[0 ] = 21;
+            System.out.print(y[0]);
+}
+}
+
+
